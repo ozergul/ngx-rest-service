80a9e05a658c77aae9a9da5870c53385c32c027c
Ozer GUL <ozer.gul@etiya.com>
2020-09-08 18:27:59 +0300

    public api

--- projects/ngx-rest-service/src/public-api.ts
+++ projects/ngx-rest-service/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './lib/ngx-rest.module';
 export * from './lib/rest-decorators/rest.decorators'
 export * from './lib/rest-decorators/rest.service'
+export * from './lib/rest-decorators/rest.helpers'
