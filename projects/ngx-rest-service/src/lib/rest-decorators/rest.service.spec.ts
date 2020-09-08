99e21c9f0fd3e6fd1941385dd3fd2e0cf68fc972
Ozer GUL <ozer.gul@etiya.com>
2020-09-08 21:16:14 +0300

    test fix

--- projects/ngx-rest-service/src/lib/rest-decorators/rest.service.spec.ts
+++ projects/ngx-rest-service/src/lib/rest-decorators/rest.service.spec.ts
@@ -2,16 +2,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { RestService } from './rest.service';
 import { HttpClientModule } from '@angular/common/http';
+import { REST_CONFIG } from '../ngx-rest.config';
 
 describe('RestService', () => {
   let service: RestService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClientModule],
+      providers: [HttpClientModule, {
+        provide: REST_CONFIG, useValue: {}
+      }],
       imports: [HttpClientModule],
     });
     service = TestBed.inject(RestService);
+    service.restConfig.apiBaseUrl = 'http://test.com'
   });
 
   it('should be created', () => {
